5a1722c0aef4a56eb5ff5f615e7f761ed33a67cf
Zaviq Alnour <181534609+ZaviqAlnour@users.noreply.github.com>
2026-01-06 10:37:06 +0600

    add inpute feture

--- counter.py
+++ counter.py
@@ -1,6 +1,10 @@
-numbers = [12, 13, 14, 2, 2 , 3, 10, 0, 1, 12, 13, 27, 50, 49, 39, 5,6,8, 8, 9, 9, 7, 40, 60, 50, 1, 2, 3, 4, 2, 1, 3, 5, 90, 100, 99, 99, 99, 35, 13, 12]
-
 from collections import Counter
+
+number = input("Numbers: ")
+numbers = []
+
+for num in number:
+    numbers.append(int(num))
 
 counts = Counter(numbers)
 
