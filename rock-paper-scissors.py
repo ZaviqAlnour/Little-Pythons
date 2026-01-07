77e5ee6c3b7d4585fd478705a867552bb10fed0d
Zaviq Alnour <181534609+ZaviqAlnour@users.noreply.github.com>
2026-01-07 12:08:31 +0600

    Fixed input validation so wrong entries don’t skip rounds; improved grammar and game flow.

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -11,9 +11,9 @@ print()
 round_choice = int(input("How many rounds do you want to play?: "))
 
 print("\nRules:")
-print("Rock win  -> +2 points")
-print("Paper win -> +1 point")
-print("Scissors win -> +3 points")
+print("• Rock win  -> +2 points")
+print("• Paper win -> +1 point")
+print("• Scissors win -> +3 points")
 print("\nType 'q' anytime to quit the game.")
 print("------------------------------------------")
 
@@ -23,16 +23,19 @@ computer_point = 0
 for round_no in range(1, round_choice + 1):
 
     print(f"\nRound {round_no}")
-    user_choice = input("Enter rock, paper, or scissors: ").lower()
 
-    if user_choice == "q":
-        print("\nYou quit the game early.")
-        break
+    while True:
+        user_choice = input("Enter rock, paper, or scissors: ").lower()
 
-    if user_choice not in valid_choices:
-        print("Invalid input. Please type one of these correctly:")
-        print(valid_choices)
-        continue
+        if user_choice == "q":
+            print("\nYou quit the game early.")
+            quit()
+
+        if user_choice in valid_choices:
+            break
+        else:
+            print("Invalid input! Please type correctly:")
+            print(valid_choices)
 
     computer_choice = random.randint(1, 3)
 
@@ -55,27 +58,27 @@ for round_no in range(1, round_choice + 1):
 
         case (1, "paper"):
             user_point += 1
-            print("You won this round! You got +1 point.")
+            print("You won this round! +1 point.")
 
         case (2, "scissors"):
             user_point += 3
-            print("You won this round! You got +3 points.")
+            print("You won this round! +3 points.")
 
         case (3, "rock"):
             user_point += 2
-            print("You won this round! You got +2 points.")
+            print("You won this round! +2 points.")
 
         case (1, "scissors"):
             computer_point += 2
-            print("Computer won this round! Computer got +2 points.")
+            print("Computer won this round! +2 points.")
 
         case (2, "rock"):
             computer_point += 1
-            print("Computer won this round! Computer got +1 point.")
+            print("Computer won this round! +1 point.")
 
         case (3, "paper"):
             computer_point += 3
-            print("Computer won this round! Computer got +3 points.")
+            print("Computer won this round! +3 points.")
 
 print("\n---------- GAME OVER ----------")
 print(f"Your total points: {user_point}")
